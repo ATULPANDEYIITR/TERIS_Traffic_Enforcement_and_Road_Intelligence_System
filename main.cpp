cb365fc7546629c5fd7ffa15f262eff03d40e4e1
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:06:04 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "TERIS_Traffic_Enforcement_and_Road_Intelligence_System" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
